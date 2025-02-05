493abfed2230bce36b00f0327863c21c6d46090e
itzmarwin <itzm4rw1n@gmail.com>
2025-02-05 16:41:04 +0530

    Update delete_edited.py

--- features/delete_edited.py
+++ features/delete_edited.py
@@ -1,10 +1,9 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-@Client.on_message(filters.group & filters.edited)
+@Client.on_edited_message(filters.group)
 async def delete_edited_message(client: Client, message: Message):
     try:
         await message.delete()
     except Exception as e:
         print(f"Error deleting edited message: {e}")
-      
