11a4f341456c4bd06bc2bd8f215c1c659274a5ba
itzmarwin <itzm4rw1n@gmail.com>
2025-02-06 01:08:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
-import os
-
-API_ID = int(os.getenv("API_ID", ""))
-API_HASH = os.getenv("API_HASH", "")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-MONGO_URL = os.getenv("MONGO_URL", "")
+# config.py (Bot configuration settings)
+API_ID = 21346925  # Replace with your API ID
+API_HASH = "908c9a085a238d1cd484a4269c887234"
+BOT_TOKEN = "7733194032:AAGSzRGA3ihA_fvngTa0h60sBkBKX6BjWaQ"
+MONGO_URL = "mongodb+srv://mimeter:lmao@cluster0.u9tk8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
