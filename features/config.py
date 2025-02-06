06b9fd45518bb498812eabc12d6906e12b2fa23d
itzmarwin <itzm4rw1n@gmail.com>
2025-02-06 16:54:29 +0530

    Update config.py

--- features/config.py
+++ features/config.py
@@ -1,4 +1,4 @@
 API_ID = 21346925
 API_HASH = "908c9a085a238d1cd484a4269c887234"
 BOT_TOKEN = "7733194032:AAGSzRGA3ihA_fvngTa0h60sBkBKX6BjWaQ"
-MONGO_URL = "mongodb+srv://mimeter:lmao@cluster0.u9tk8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+PORT = 8080
